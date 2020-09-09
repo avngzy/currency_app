dfba3870433c9f19ccd303d8335632425e62311f
Alvin Gooi <alvingooi@gmail.com>
2020-09-09 16:26:45 +0800

    inital project creation

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Bee Diu on 03/09/2020.
+//  Copyright © 2020 Bee Diu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
